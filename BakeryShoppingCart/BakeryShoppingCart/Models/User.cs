bf7ee23f4cbb00a9e69cfffd5b59adb7cb2d72da
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-01-20 11:37:43 -0700

    Add files via upload

---
+++ BakeryShoppingCart/BakeryShoppingCart/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BakeryShoppingCart.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        private string EncryptedPassword { get; set; }
+
+        public string Email { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
---
+++ BakeryShoppingCart/BakeryShoppingCart/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BakeryShoppingCart.Models;
+using BakeryShoppingCart.DTOS;
+
+namespace BakeryShoppingCart
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime Date1 = new DateTime(1983, 2, 20);
+            DateTime Date2 = new DateTime(1999, 5, 15);
+
+            int result = DateTime.Compare(Date1, Date2);
+
+            Console.WriteLine(result);
+
+            if (Date2 > Date1)
+            {
+                Console.WriteLine("The Date is Greater!");
+            }
+            else
+            {
+                Console.WriteLine("The date is less than!");
+            }
+            
+
+           
+        }
+    }
+}
